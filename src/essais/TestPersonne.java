95042e0acfcde51b5310141969fda3112efe7a7e
chikaakta <59062753+chikaakta@users.noreply.github.com>
2021-02-08 15:28:44 +0100

    working

--- src/essais/TestPersonne.java
+++ src/essais/TestPersonne.java
@@ -5,8 +5,7 @@ import entites.Personne;
 
 public class TestPersonne {
     public static void main(String[] args){
-        AdressePostale adressePostale = new AdressePostale(12, "rue juir", 75015, "PAris");
-        Personne jean = new Personne("Jean", "popl", adressePostale);
+        Personne jean = new Personne("Jean", "popl", new AdressePostale(12, "rue juir", 75015, "PAris"));
         System.out.println(jean.adressePostale.codePostal);
     }
 }
